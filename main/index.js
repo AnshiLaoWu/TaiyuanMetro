85b0417f7edb94b7a0f4605f2b36527d66b92f02
AnshiLaoWu <150592244+AnshiLaoWu@users.noreply.github.com>
2025-01-18 19:48:38 +0800

    Add files via upload

---
+++ main/index.js
@@ -0,0 +1,12 @@
+window.history.replaceState(null,null,window.location.href);
+const query = document.getElementById("searchInput").value;
+    const data = [
+        {"id":1,"title":"太原地铁乘客守则","content":"为规范城市轨道交通管理，保障城市轨道交通安全运营..."},
+        {"id":2,"title":"太原地铁乘车指南(1.0)","content":"一、太原地铁概述 太原是中国煤业大省——山西的省会城市..."},
+        {"id":3,"title":"太原地铁乘车指南(2.0)","content":"一、太原地铁概述 太原是中国煤业大省——山西的省会城市..."},
+        {"id":4,"title":"新闻速递|太原地铁1号线车站命名公示方案","content":"太原市城市轨道交通1号线一期工程线路全长28.737公里..."},
+        {"id":5,"title":"新闻速递|太原地铁1号线首列列车下线","content":"1月4日下午2时许，随着一声嘹亮的汽笛声..."},
+    ];
+function search(){
+
+}
