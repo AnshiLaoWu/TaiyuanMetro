48729c2a8642f1ea64f6ca4150459612790d2f8b
Zaak Tung Wu <laowuservice@163.com>
2025-05-10 22:08:05 +0800

    Add files via upload

--- mobile/index.js
+++ mobile/index.js
@@ -88,3 +88,12 @@ window.onclick = function(event) {
     }
   }
 }
+$.get('../main/text_wrap.txt')
+  .done(function(data) {
+    var lines = data.split("\n");
+    var content = lines.join("\n");
+    document.getElementById('text_wrap').innerText = content;
+  })
+  .fail(function() {
+    document.getElementById('text_wrap').innerText = "加载文件失败！";
+  });
